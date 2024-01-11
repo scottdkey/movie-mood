4a2f2a246c821b9acefcc4bdfb02b4e4d4fe4e9f
Scott Key <scottdkey@gmail.com>
2024-01-11 00:18:43 -0700

    logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,17 @@
 import type { Handle } from '@sveltejs/kit';
 import { theme, type ThemeVal } from './lib/stores/theme';
 
+export let requestIp = ''
+export let correlationId = crypto.randomUUID()
+
 export const handle = (async ({ event, resolve }) => {
+
+	try {
+		requestIp = event.request.headers.get("x-forwarded-for") || event.getClientAddress();
+  } catch (error) {
+    console.error(error, 'Error reading IP');
+  }
+
 	event.locals.theme = event.cookies.get('theme') as ThemeVal;
 	theme.set(event.locals.theme);
 	return resolve(event);
--- src/routes/api/movie/[slug]/+server.ts
+++ src/routes/api/movie/[slug]/+server.ts
@@ -3,8 +3,9 @@ import {
 	getMovieById,
 	getMovieWatchProvidersByMovieId,
 } from '../../../../lib/server/moviedb.requests';
+import { requestIp } from '../../../../hooks.server';
 
-export const GET: RequestHandler = async ({ params, getClientAddress }) => {
+export const GET: RequestHandler = async ({ params  }) => {
 	console.log(params);
 	const slug = params['slug'];
 	let movieId = '872585-oppenheimer';
@@ -13,7 +14,7 @@ export const GET: RequestHandler = async ({ params, getClientAddress }) => {
 	}
 	const movieInfo = await getMovieById(movieId);
 	const providerInfo = await getMovieWatchProvidersByMovieId(movieId);
-	console.log(getClientAddress())
+	console.log(requestIp, "requestIp")
 
 
 
